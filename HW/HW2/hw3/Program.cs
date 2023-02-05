22be376290dbc5c9fb240186a5281d52d6810187
Doushy <dimasicaa@yandex.ru>
2023-02-05 21:54:10 +0300

    add hw3

---
+++ HW/HW2/hw3/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход
+// цифру, обозначающую день недели, и проверяет, является ли
+// этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+
+Console.Clear();
+System.Console.WriteLine($"Введите цифру от 1 - 7");
+int num = int.Parse(Console.ReadLine());
+if ((num == 6) || (num == 7))
+{
+    System.Console.WriteLine($"{num} -> Да");
+}
+else
+{
+    System.Console.WriteLine($"{num} -> Нет");
+}
+
+
+
+
+
+
+
+
+
+
+
+
