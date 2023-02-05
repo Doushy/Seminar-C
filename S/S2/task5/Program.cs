657f04c2377ea968bae451506269017953049e07
Doushy <dimasicaa@yandex.ru>
2023-02-05 18:27:48 +0300

    add task 5

---
+++ S/S2/task5/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+Console.Clear();
+
+int count = 1;
+while(count <= 100)
+{
+    if(( count % 3 == 0) && ( count % 5 == 0))
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        System.Console.Write("FizzBuzz");
+    }else if( count % 3 == 0) 
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        System.Console.Write("Fizz");
+    }else if( count % 5 == 0) 
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        System.Console.Write("Buzz");
+    }else  
+    {
+        System.Console.Write(count);
+    }
+    Console.ForegroundColor = ConsoleColor.White;
+    if( count == 100) 
+    {
+        System.Console.Write(".");
+    }else  
+    {
+        System.Console.Write(", ");
+    }
+    count++;
+}
