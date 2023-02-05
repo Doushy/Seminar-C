9fe08926138469cd5f2e0485e9a4bb90e0171967
Doushy <dimasicaa@yandex.ru>
2023-02-05 20:16:30 +0300

    add hw1

---
+++ HW/HW2/hw1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход
+// трёхзначное число и на выходе показывает вторую цифру этого
+// числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Clear();
+System.Console.WriteLine($"Введите ТРЁХЗНАЧНОЕ число");
+int num = int.Parse(Console.ReadLine());
+int rem = num.ToString().Length;
+int a1 = 0;
+if (rem == 3)
+{
+    a1 = num / 10 % 10;
+    System.Console.WriteLine($"{num} -> {a1}");
+    }
+else
+{
+System.Console.WriteLine($"Вы ввели {rem} значное число");
+}
+
+
+
+
+
+
