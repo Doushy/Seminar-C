bce3a7c63b418085732fd46e4a58013f4b3930c8
Doushy <dimasicaa@yandex.ru>
2023-02-05 18:04:21 +0300

    add task 3

---
+++ S/S2/task3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход
+// число и проверяет, кратно ли оно одновременно
+// 7 и 23.
+// ● 14 -> нет
+// ● 46 -> нет
+// ● 161 -> да
+
+Console.Clear();
+System.Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+
+if(( num % 7 == 0) && ( num % 23 == 0))
+{
+    System.Console.WriteLine($"{num} -> да");    
+}
+else
+{
+    System.Console.WriteLine($"{num} -> нет");
+}
