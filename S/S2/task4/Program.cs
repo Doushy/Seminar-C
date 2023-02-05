dd3d6631d7f984ceb159ecddb4df1c155fd440ba
Doushy <dimasicaa@yandex.ru>
2023-02-05 18:07:26 +0300

    add task 4

---
+++ S/S2/task4/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход два
+// числа и проверяет, является ли одно число квадратом
+// другого.
+// ● 5, 25 -> да
+// ● -4, 16 -> да
+// ● 25, 5 -> да
+// ● 8,9 -> нет 
+
+Console.Clear();
+System.Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+System.Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+
+
+if(( num1 * num1 == num2) || (num2 * num2 == num1))
+{
+    System.Console.WriteLine($"{num1}, {num2} -> да");    
+}
+else
+{
+    System.Console.WriteLine($"{num1}, {num2}  -> нет");
+}
