eed0ba7c5e2ea521d6587b0137f62d6547df67cf
Сторчеус Дмитрий Александрович <d.storcheus@rusgazburenie.ru>
2023-02-07 16:44:56 +0300

    add s3 task3

---
+++ S/S3/task2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача №22.Работа в группах
+// Напишите программу, которая принимает на вход
+// число (N) и выдаёт таблицу квадратов чисел
+// от 1 до N.
+// ● 5 -> 1, 4, 9, 16, 25.
+// ● 2 -> 1,4
+
+
+Console.Clear();
+System.Console.Write("Ведите число ");
+int x1 = int.Parse(Console.ReadLine()!)!;
+
+
+double result = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) + (y2 - y1));
+System.Console.WriteLine(result);
+
+
+
+
---
+++ S/S3/task3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход
+// число (N) и выдаёт таблицу квадратов чисел
+// от 1 до N.
+// ● 5 -> 1, 4, 9, 16, 25.
+// ● 2 -> 1,4
+
+Console.Clear();
+System.Console.Write("Ведите число ");
+int num = int.Parse(Console.ReadLine()!)!;
+int counter = 1;
+
+while (counter < num)
+{
+    Console.Write(counter * counter + ", ");
+    counter++;
+}
+Console.WriteLine(counter * counter + ".");
+
+
+
+
+
+
