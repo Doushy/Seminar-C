a526d26ac907da4cd4c08e8cae282bf7ff9eefa4
Сторчеус Дмитрий Александрович <d.storcheus@rusgazburenie.ru>
2023-02-07 16:12:00 +0300

    add s3 task1

---
+++ S/S3/task1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая по заданному номеру
+// четверти, показывает диапазон возможных координат
+// точек в этой четверти (x и y).
+
+Console.Clear();
+System.Console.Write("Ведите номер четверти: ");
+long x1 = long.Parse(Console.ReadLine()!)!;
+System.Console.Write("Ведите номер четверти: ");
+long x2 = long.Parse(Console.ReadLine()!)!;
+System.Console.Write("Ведите номер четверти: ");
+long y1 = long.Parse(Console.ReadLine()!)!;
+System.Console.Write("Ведите номер четверти: ");
+long y2 = long.Parse(Console.ReadLine()!)!;
+
+double result = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) + (y2 - y1));
+System.Console.WriteLine(result);
+
+
+
+
