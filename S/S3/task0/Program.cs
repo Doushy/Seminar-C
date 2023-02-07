0a9b0df1d6d8f66905c5da6b3b2cce2f3fccdb2d
Сторчеус Дмитрий Александрович <d.storcheus@rusgazburenie.ru>
2023-02-07 13:31:58 +0300

    add s3 task0

---
+++ S/S3/task0/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая по заданному номеру
+// четверти, показывает диапазон возможных координат
+// точек в этой четверти (x и y).
+
+Console.Clear();
+System.Console.Write("Ведите номер четверти: ");
+int quarter = int.Parse(Console.ReadLine());
+if ((quarter > 4) || (quarter < 1))
+{
+    System.Console.Write("Нет такой чертверти");
+}
+else if (quarter == 1)
+{
+    System.Console.Write("X>0;Y>0");
+}
+else if (quarter == 2)
+{
+    System.Console.Write("X<0;Y>0");
+}
+else if (quarter == 3)
+{
+    System.Console.Write("X<0;Y<0");
+}
+else
+{
+    System.Console.Write("X>0;Y<0");
+}
