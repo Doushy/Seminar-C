641e71f2ad19088957bdce7f27216a8b6693c518
Сторчеус Дмитрий Александрович <d.storcheus@rusgazburenie.ru>
2023-02-09 13:04:49 +0300

    add hw2

---
+++ HW/HW3/hw2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+System.Console.Write("Ведите x1: ");
+long x1 = long.Parse(Console.ReadLine()!)!;
+System.Console.Write("Ведите y1: ");
+long x2 = long.Parse(Console.ReadLine()!)!;
+System.Console.Write("Ведите x2: ");
+long y1 = long.Parse(Console.ReadLine()!)!;
+System.Console.Write("Ведите y2: ");
+long y2 = long.Parse(Console.ReadLine()!)!;
+System.Console.Write("Ведите x3: ");
+long z1 = long.Parse(Console.ReadLine()!)!;
+System.Console.Write("Ведите y3: ");
+long z2 = long.Parse(Console.ReadLine()!)!;
+
+double result = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) + (z2 - z1) * (z2 - z1));
+System.Console.WriteLine(result);
---
+++ HW/HW3/hw2_1/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] vars1, vars2;
+int pars1, pars2;
+System.Console.WriteLine("Введите числа для x1 y1 z1 через пробел");
+vars1 = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(i => int.TryParse(i, out pars1) ? pars1 : 0).ToArray();
+System.Console.WriteLine("Введите числа для x2 y2 z2 через пробел");
+vars2 = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(i => int.TryParse(i, out pars2) ? pars2 : 0).ToArray();
+
+double result = Math.Round(Math.Sqrt((vars2[0] - vars1[0]) * (vars2[0] - vars1[0]) + (vars2[1] - vars1[1]) * (vars2[1] - vars1[1]) + (vars2[2] - vars1[2]) * (vars2[2] - vars1[2])), 2);
+
+System.Console.WriteLine(result);
