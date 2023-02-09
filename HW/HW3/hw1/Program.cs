3029b86395e266ddd2c2b8511a5aee579ecd6762
Сторчеус Дмитрий Александрович <d.storcheus@rusgazburenie.ru>
2023-02-09 11:34:51 +0300

    add hw1 task0

--- HW/HW3/hw1/Program.cs
+++ HW/HW3/hw1/Program.cs
@@ -1,38 +1,40 @@
 ﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 
 // 14212 -> нет
-
 // 12821 -> да
-
 // 23432 -> да
 
 Console.Clear();
-System.Console.Write("Ведите НЕЧЁТНОЕ число. '123 или 12345 и т.д.' ");
+System.Console.Write("Ведите число от 10 и более: ");
 double num = int.Parse(Console.ReadLine()!)!;
 double a1, a2 = 1;
 double length = num.ToString().Length;
 double number = num;
-if ((length % (2 * Math.Floor(length / 2))) == 1)
+
+if (length >= 2)
 {
     int count = 0;
     bool test = true;
-    while ((count < (num.ToString().Length - 2)) && (test = true))
+    while ((count <= (num.ToString().Length)) && (test == true))
     {
-        a1 = Math.Round(num / (Math.Pow(10, (num.ToString().Length)) / 10), 0);
+        a1 = Math.Floor(num / (Math.Pow(10, ((num.ToString().Length)) - 1)));
         a2 = num % 10;
-
-        num = Math.Round((num % (Math.Pow(10, num.ToString().Length - 1)) / 10), 0);
+        //System.Console.WriteLine($"{a1} -> {a2}");   test
+        num = Math.Floor((num % (Math.Pow(10, num.ToString().Length - 1)) / 10));
+        //System.Console.WriteLine($"{num}");   test
 
         if (a1 == a2)
         { test = true; }
         else { test = false; }
-        count = count + 2;
+        count = count + 1;
     }
+    // System.Console.WriteLine($"{test} ");   test
     if (test == true)
     { System.Console.WriteLine($"{number} -> Да"); }
     else { System.Console.WriteLine($"{number} -> Нет"); }
 }
 else
 {
-    System.Console.WriteLine("Ввели ЧЕТНОКОЛЛИЧЕСТВЕННОЕ. Введите 3-n,5-n и т д");
+    System.Console.WriteLine("Введите другое число.");
 }
+
