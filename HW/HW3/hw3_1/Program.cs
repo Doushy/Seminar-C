b5be48467c12eb428cc11ef2c643b35279daa80d
Сторчеус Дмитрий Александрович <d.storcheus@rusgazburenie.ru>
2023-02-09 15:43:43 +0300

    add hw3

---
+++ HW/HW3/hw3_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+System.Console.Write("Ведите число ");
+int num = int.Parse(Console.ReadLine()!)!;
+int counter = 1;
+
+while (counter < num)
+{
+    Console.Write((Math.Pow(counter, (3))) + ", ");
+    counter++;
+}
+Console.WriteLine((Math.Pow(counter, (3))) + ".");
