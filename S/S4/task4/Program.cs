8717684840a7b61d9efb28854b231b1b8b08c758
Doushy <dimasicaa@yandex.ru>
2023-02-12 22:28:38 +0300

    add S4

--- S/S4/task4/Program.cs
+++ S/S4/task4/Program.cs
@@ -1,6 +1,39 @@
-﻿Console.Clear;
+﻿Console.Clear();
 
-string text = Console.ReadLine();
+int[] arr = new int[8];
+int[] arr2 = new int[8];
+int[] sumArr = new int[8];
+RandomArray(arr);
+RandomArray(arr2);
+PrintArray(arr);
+PrintArray(arr2);
+System.Console.WriteLine("сумма");
+SumArr(arr, arr2);
+PrintArray(sumArr);
 
-int num = new Random().Next(10, 100);
-char[] obrtext = text.ToCharArray();
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < 8; i++)
+    {
+        System.Console.Write(array[i] + ", ");
+    }
+    System.Console.WriteLine();
+}
+
+void RandomArray(int[] array)
+{
+    for (int i = 0; i < 8; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+    }
+}
+
+void SumArr(int[] array1, int[] array2)
+{
+    for (int i = 0; i < 8; i++)
+    {
+        sumArr[i] = array1[i] + array2[i];
+    }
+
+}
