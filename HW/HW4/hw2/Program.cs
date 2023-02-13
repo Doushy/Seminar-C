9ecb3571f738ff820054c9fccd586313a3062407
Сторчеус Дмитрий Александрович <d.storcheus@rusgazburenie.ru>
2023-02-13 17:18:34 +0300

    add hw2

---
+++ HW/HW4/hw2/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+
+int Exponentiate(int num)
+{
+    int result = 0;
+    while (num > 0)
+    {
+        result += num % 10;
+        num = num / 10;
+    }
+    return result;
+}
+
+int num = Prompt("Число: ");
+System.Console.WriteLine($"Сумма числа '{num}' = {Exponentiate(num)}");
+
+
+
+
+
