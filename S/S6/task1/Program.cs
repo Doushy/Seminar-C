7b4b89a7cfb8b6496a5746e166dabdea3eb5b523
Doushy <dimasicaa@yandex.ru>
2023-02-13 23:55:48 +0300

    add S6

---
+++ S/S6/task1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три 
+// числа и проверяет, может ли существовать треугольник с сторонами 
+// такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника 
+// меньше суммы двух других сторон.
+
+int askNumber(string msg)
+{
+    System.Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine());
+}
+
+int num1 = askNumber("Первое: ");
+int num2 = askNumber("Второе: ");
+int num3 = askNumber("Третье: ");
+
+if (num2 + num3 > num1 && num1 + num3 > num2 && num2 + num3 > num1)
+{
+    System.Console.WriteLine("Треугольник может существовать");
+}
+else
+{
+    System.Console.WriteLine("Треугольник не может существовать");
+}
---
+++ S/S6/task2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать 
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+int askNumber(string msg)
+{
+    System.Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine());
+}
+
+int num = askNumber("число: ");
+
+string Binary(int num)
+{
+    int notation = 2; //система счисления
+    string sum = String.Empty;
+    while (num > 0)
+    {
+        sum = num % notation + sum;
+        num /= notation;
+    }
+    return sum;
+}
+
+System.Console.WriteLine(Binary(num));
+
