9845416dec354e9018c765d0fb4c6172541c6098
Сторчеус Дмитрий Александрович <d.storcheus@rusgazburenie.ru>
2023-02-14 15:07:35 +0300

    add hw2

---
+++ HW/HW5/hw2/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6]-> 0
+
+Console.Clear();
+
+int[] array = GetArray(4, 0, 10);
+System.Console.Write($"[{String.Join(", ", array)} ] -> ");
+System.Console.Write(EvenNumber(array));
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+int EvenNumber(int[] array)
+{
+    int min = array[0];
+    int max = array[0];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+    }
+    int result = max - min;
+    return result;
+}
