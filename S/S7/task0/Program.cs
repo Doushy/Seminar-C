654c5ed6db94cea16ecbc4a35a11df21a79308c2
Сторчеус Дмитрий Александрович <d.storcheus@rusgazburenie.ru>
2023-02-21 15:47:38 +0300

    add S7 task0

---
+++ S/S7/task0/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+int rows = int.Parse(Console.ReadLine());
+
+int columns = int.Parse(Console.ReadLine());
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            System.Console.Write($"{inArray[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
