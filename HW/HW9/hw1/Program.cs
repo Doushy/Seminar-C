6add08de4e3ea24a13788466195ab7864e20235b
Doushy <dimasicaa@yandex.ru>
2023-03-05 20:39:22 +0300

    add HW9 hw1

---
+++ HW/HW9/hw1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Write("Введите N:");
+int UserN = int.Parse(Console.ReadLine()!)!;
+
+NaturalNumbers(UserN);
+Console.WriteLine();
+
+void NaturalNumbers(int start, int n = 1)
+{
+    if (start >= n)
+    {
+        Console.Write($" {start}");
+        if (start != n) Console.Write(","); else Console.WriteLine(".");
+        NaturalNumbers(start - 1, n);
+    }
+}
